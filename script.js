483a0fd8babc40a335133ea268562ecccc87e703
Neto <tabalneto14@gmail.com>
2022-12-13 17:01:19 -0400

    grid finished, but static.

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ const div1 = document.getElementById('container');
 
 for (let index = 0; index < 16; index++) {
     var p1 = document.createElement('p');
-    var p1content = document.createTextNode(index);
-    p1.appendChild(p1content);
     var grid = document.createElement('div');
     grid.appendChild(p1);
     div1.appendChild(grid);
@@ -14,7 +12,7 @@ for (let index = 0; index < 16; index++) {
         div1.appendChild(grid2);
         grid2.className = 'gridSize';
     }
-    
+
 }
 
 
