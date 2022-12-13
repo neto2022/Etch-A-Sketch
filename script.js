d9eeea568157a2e5af798107472488be61858286
Neto <tabalneto14@gmail.com>
2022-12-13 15:39:58 -0400

    grid generator test

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-let div1 = document.getElementById(container);
+const div1 = document.getElementById('container');
+
+for (let index = 0; index < 16; index++) {
+    var grid = document.createElement('div');
+    div1.appendChild(grid);
+    grid.className = 'gridSize';
+}
+
+for (let index = 0; index < 16; index++) {
+    var grid2 = document.createElement('div');
+    grid.appendChild(grid2);
+    grid2.className = 'gridSize';
+}
